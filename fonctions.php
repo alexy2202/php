611ef4ac8f66e6dca0b269b83d290c1d7ffc2be2
Forient Alexy <alexyforient76@gmail.com>
2022-12-01 09:56:42 +0100

    fonctions.php

---
+++ fonctions.php
@@ -0,0 +1,14 @@
+<?php
+$texte = ucfirst ("bonjour tout le monde");
+
+echo $texte."<br>"; 
+
+function bonjour($nom,$prenom)
+{
+    echo "bonjour ".$nom." ".$prenom;
+}
+
+bonjour("bon","jean");
+
+
+?>
